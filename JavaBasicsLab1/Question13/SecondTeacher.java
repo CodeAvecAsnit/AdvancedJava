0708d547a235a482bde173c558a886081d9b7743
CodeAvecAsnit <asnitbakhati@gmail.com>
2025-12-25 22:09:33 +0545

    Multithreading concepts made clear

---
+++ JavaBasicsLab1/Question13/SecondTeacher.java
@@ -0,0 +1,18 @@
+package JavaBasicsLab1.Question13;
+
+import java.security.SecureRandom;
+
+public class SecondTeacher extends Thread{
+
+    @Override
+    public void run() {
+        SecureRandom sr = new SecureRandom();
+        while (true) {
+            System.out.println(sr.nextInt(100)+"R");
+            try {
+                Thread.sleep(1000);
+            } catch (Exception exception) {
+            }
+        }
+    }
+}
---
+++ JavaBasicsLab1/Question9/Main.java
@@ -0,0 +1,17 @@
+package JavaBasicsLab1.Question9;
+
+public class Main {
+    public static void main(String[] args) {
+        StaticNested.Node n1 = new StaticNested.Node();
+        n1.incrementDisplay();
+        n1.incrementDisplay();
+
+        System.out.println();
+        NonStaticNested nsTed = new NonStaticNested();
+        NonStaticNested.Inner in = nsTed.new Inner();
+        in.display();
+        in.display();
+        in.display();
+        in.whatsNew("Question number 9 ");
+    }
+}
